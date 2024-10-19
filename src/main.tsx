5ce6563ad451954dde6fd95d73a537d73d920604
dot-enny <olaniyaneniola0225@gmail.com>
2024-10-19 14:50:08 +0100

    feat: sign-in

--- src/main.tsx
+++ src/main.tsx
@@ -7,17 +7,21 @@ import { SignIn } from './pages/SignIn.tsx'
 import { SignUp } from './pages/SignUp.tsx'
 import { AuthLayout } from './layouts/Auth.tsx'
 import { Notification } from './components/notification/Notification.tsx'
+import { ProtectedRoute } from './components/ProtectedRoute.tsx'
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { 
-    path: '/auth', 
+  {
+    path: '/',
+    element: <ProtectedRoute><Home /></ProtectedRoute>
+  },
+  {
+    path: '/auth',
     element: <AuthLayout />,
     children: [
       { path: 'sign-in', element: <SignIn />, },
       { path: 'sign-up', element: <SignUp /> },
     ]
-   },
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
