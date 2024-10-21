43d6f180b221dda8d597cafd7ebf0d8fa4d9f74a
dot-enny <olaniyaneniola0225@gmail.com>
2024-10-21 16:14:42 +0100

    feat: displays user avatar and username fetched from db.

--- src/components/list/userInfo/UserInfo.tsx
+++ src/components/list/userInfo/UserInfo.tsx
@@ -1,13 +1,17 @@
+import { useUserStore } from "../../../lib/userStore";
 import { IconCamera } from "../../icons/IconCamera"
 import { IconEdit } from "../../icons/IconEdit"
 import { IconMore } from "../../icons/IconMore"
 
 export const UserInfo = () => {
+
+  const { currentUser } = useUserStore();
+
   return (
     <div className="p-5 flex items-center justify-between">
         <div className="flex items-center gap-5">
-            <img src="./img/avatar-placeholder.png" alt="user" className="w-12 h-12 rounded-full object-cover" />
-            <h2>John Doe</h2>
+            <img src={ currentUser.avatar || '/img/avatar-placeholder.png'} alt="user" className="w-12 h-12 rounded-full object-cover" />
+            <h2>{ currentUser.username || 'John Doe'}</h2>
         </div>
         <div className="flex gap-5">
             <IconMore />
