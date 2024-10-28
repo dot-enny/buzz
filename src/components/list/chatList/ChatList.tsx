bd3e0913a33fc36fb0f624933dbd15e3a8a1e11c
dot-enny <olaniyaneniola0225@gmail.com>
2024-10-28 17:48:12 +0100

    feat: hide username and profile picture of blocked user on chatlist component.

--- src/components/list/chatList/ChatList.tsx
+++ src/components/list/chatList/ChatList.tsx
@@ -87,6 +87,8 @@ export const ChatList = () => {
 
 const ListItem = ({ chat, onClick }: { chat: any, onClick: () => void }) => {
     const sender = chat.user;
+    const { currentUser } = useUserStore();
+    const userBlocked = sender.blocked.includes(currentUser.id);
     
     return (
         <div onClick={onClick} className="flex items-center gap-5 p-5 cursor-pointer border-b border-b-gray-800"
@@ -94,9 +96,9 @@ const ListItem = ({ chat, onClick }: { chat: any, onClick: () => void }) => {
                 backgroundColor: chat.isSeen ? 'transparent' : 'rgba(255, 255, 255, 0.1)'    
             }}
         >
-            <img src={ sender.avatar || './img/avatar-placeholder.png'} alt="user" className="w-12 h-12 rounded-full object-cover" />
+            <img src={ userBlocked ? sender.avatar : './img/avatar-placeholder.png'} alt="user" className="w-12 h-12 rounded-full object-cover" />
             <div>
-                <h2>{ sender.username }</h2>
+                <h2>{ userBlocked ? sender.username : 'User' }</h2>
                 <p className="text-neutral-500">{ chat.lastMessage }</p>
             </div>
         </div>
