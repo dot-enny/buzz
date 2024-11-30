d657420cb6bd22e783318f572cbb5d213cbceda8
Enny <109947938+dot-enny@users.noreply.github.com>
2024-11-30 01:40:02 +0100

    fix: deployment error.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
@@ -7,14 +7,17 @@ import { getStorage } from "firebase/storage"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: "react-chat-app-72301.firebaseapp.com",
-//   projectId: "react-chat-app-72301",
-//   storageBucket: "react-chat-app-72301.appspot.com",
-//   messagingSenderId: "696950571746",
-//   appId: "1:696950571746:web:f0b072dd7a6307164054bb"
-// };
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: "react-chat-app-72301.firebaseapp.com",
+  projectId: "react-chat-app-72301",
+  storageBucket: "react-chat-app-72301.appspot.com",
+  messagingSenderId: "696950571746",
+  appId: "1:696950571746:web:f0b072dd7a6307164054bb"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore();
