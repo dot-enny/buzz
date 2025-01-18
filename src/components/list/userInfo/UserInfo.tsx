51d0f6af3c201195cf4f81d91e33c7ecc529e457
dot-enny <olaniyaneniola0225@gmail.com>
2025-01-18 16:09:31 +0100

    feat: update profile.

--- src/components/list/userInfo/UserInfo.tsx
+++ src/components/list/userInfo/UserInfo.tsx
@@ -1,23 +1,27 @@
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { useUserStore } from "../../../lib/userStore";
-import { IconCamera } from "../../icons/IconCamera"
-import { IconEdit } from "../../icons/IconEdit"
-import { IconMore } from "../../icons/IconMore"
+import Tooltip from "../../ui/Tooltip";
+import { useState } from "react";
+import EditInfo from "./EditInfo";
 
 export const UserInfo = () => {
 
   const { currentUser } = useUserStore();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="p-5 flex items-center justify-between">
         <div className="flex items-center gap-5">
-            <img src={ currentUser.avatar || '/img/avatar-placeholder.png'} alt="user" className="w-12 h-12 rounded-full object-cover" />
-            <h2>{ currentUser.username || 'John Doe'}</h2>
+            <img src={ currentUser.avatar } alt="user" className="w-12 h-12 rounded-full object-cover" />
+            <span className="">
+              <h2>{ currentUser.username }</h2>
+              <span className="text-xs text-gray-400">{ currentUser.status }</span>
+            </span>
         </div>
-        <div className="flex gap-5">
-            <IconMore />
-            <IconCamera />
-            <IconEdit />
-        </div>
+        <Tooltip tip="edit profile" className="mt-2 -ml-7">
+          <PencilSquareIcon className="text-white size-5 cursor-pointer" onClick={() => (setIsOpen((prev) => !prev))} /> 
+        </Tooltip>
+        <EditInfo isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
   )
 }
