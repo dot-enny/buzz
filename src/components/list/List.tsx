2803115cd8a6414bcf9845be6d07ca4954bf1289
dot-enny <olaniyaneniola0225@gmail.com>
2025-01-28 10:53:09 +0100

    refactor: logout functionality, moved to custom hook and is now on list component for smaller screens.

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,11 +1,19 @@
+import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
 import { ChatList } from "./chatList/ChatList"
 import { UserInfo } from "./userInfo/UserInfo"
+import { useSignOut } from "../../hooks/useSignOut";
 
 export const List = () => {
+
+  const { signOut } = useSignOut();
+
   return (
     <div className="flex-1 flex flex-col h-screen">
       <UserInfo />
       <ChatList />
+      <button onClick={signOut} className="xl:hidden mt-1 w-fit mx-auto fixed bottom-5 left-5 rounded-full border border-red-300 p-3">
+        <ArrowLeftStartOnRectangleIcon className="text-red-500 size-6" />
+      </button>
     </div>
   )
 }
