e5937f382b309853e1592004c7c00db3ca8d5530
dot-enny <olaniyaneniola0225@gmail.com>
2025-10-14 17:34:27 +0100

    fix(ui): multiple userinfo components.

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,6 +1,5 @@
 import { ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/outline";
 import { ChatList } from "./chatList/ChatList"
-import { UserInfo } from "./userInfo/UserInfo"
 import { useSignOut } from "../../hooks/useSignOut";
 
 export const List = () => {
@@ -8,8 +7,8 @@ export const List = () => {
   const { signOut } = useSignOut();
 
   return (
-    <div className="min-h-[100dvh] max-h-[100dvh]">
-      <UserInfo />
+    <div className="min-h-[100dvh] max-h-[100dvh] overflow-y-auto overflow-x-clip">
+      {/* <UserInfo /> */}
       <ChatList />
       <button onClick={signOut} className="xl:hidden mt-1 w-fit mx-auto fixed bottom-5 left-5 rounded-full border border-red-300 p-3">
         <ArrowLeftStartOnRectangleIcon className="text-red-500 size-6" />
