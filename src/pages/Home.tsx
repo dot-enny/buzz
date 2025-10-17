8f4bd87a20e30883fc18656518c6c61d3a54a4ee
dot-enny <olaniyaneniola0225@gmail.com>
2025-10-17 15:47:22 +0100

    feat(ui): chat component slides into view.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,11 +9,16 @@ export default function Home() {
   const { isChatOpen } = useAppStateStore();
 
   return (
-    <main className="md:flex">
+    <main className="max-h-screen overflow-clip md:flex relative">
       <div className={`flex-1 ${isChatOpen && 'max-md:hidden'}`}>
         <List />
       </div>
-      <div className={`${!isChatOpen && 'max-md:hidden'} md:flex-1 lg:flex-[2] min-h-screen`}>
+      <div className={`
+        md:flex-1 lg:flex-[2] min-h-screen
+        max-md:fixed max-md:inset-0 max-md:z-10 max-md:bg-neutral-950
+        max-md:transition-transform max-md:duration-300 max-md:ease-in-out
+        ${!isChatOpen ? 'max-md:translate-x-full' : 'max-md:translate-x-0'}
+      `}>
         <Chat />
       </div>
       <div className="max-xl:hidden flex-1">
